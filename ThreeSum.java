17c84b383614a3a79c7956ea4731513ac42c8e63
Pranjal Chakraborty <pranjalchakrabortybabu@gmail.com>
2022-11-07 18:19:13 -0500

    Create ThreeSum.java

---
+++ ThreeSum.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> solution = new HashSet();
+        
+        if (nums.length < 3) {
+            return Collections.emptyList();
+        }
+        
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            int j = i + 1;
+            int k = nums.length - 1;
+            
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                
+                if (sum < 0) {
+                    j++;
+                    
+                } else if (sum > 0) {
+                    k--;
+                    
+                } else {
+                    solution.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                }
+            }
+        }
+        
+        return new ArrayList(solution);
+    }
+}
