3432e0d6d4a1a28093ed5a1e36adfe4f280d394e
Pranjal Chakraborty <pranjalchakrabortybabu@gmail.com>
2022-11-10 11:26:52 -0500

    Create ValidParentheses.java

---
+++ ValidParentheses.java
@@ -0,0 +1,32 @@
+// Readable? Absolutely yes. Precise? Nope, Do I care? Nope!
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c == '[' || c == '{' || c == '(') {
+                stack.push(c);
+                
+            } else if (stack.isEmpty()) {
+                return false;
+                
+            } else if (c == ']' && stack.peek() != '[') {
+                return false;
+                
+            } else if (c == '}' && stack.peek() != '{') {
+                return false;
+                
+            } else if (c == ')' && stack.peek() != '(') {
+                return false;
+                
+            } else {
+                stack.pop();
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
